4900e3db6eaf34a645c016365d119c8789dbc30b
vboxuser <vboxuser@ubuntu-server-01.myguest.virtualbox.org>
2023-12-08 10:25:53 +0900

    up

--- docker_compose/api/cruds/task.py
+++ docker_compose/api/cruds/task.py
@@ -1,8 +1,12 @@
 from typing import List, Tuple, Optional
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 from sqlalchemy import select
 from sqlalchemy.engine import Result
+
+import api.models.task as task_model
+import api.schemas.task as task_schema
 
 
 async def get_tasks_with_done(db: AsyncSession) -> List[Tuple[int, str, bool]]:
@@ -25,6 +29,15 @@ async def get_task(db: AsyncSession, task_id: int) -> Optional[task_model.Task]:
     task: Optional[Tuple[task_model.Task]] = result.first()
     return task[0] if task is not None else None  # 要素が一つであってもtupleで返却されるので１つ目の要素を取り出す
 
+
+async def create_task(
+    db: AsyncSession, task_create: task_schema.TaskCreate
+) -> task_model.Task:
+    task = task_model.Task(**task_create.dict())
+    db.add(task)
+    await db.commit()
+    await db.refresh(task)
+    return task
 
 async def update_task(
     db: AsyncSession, task_create: task_schema.TaskCreate, original: task_model.Task
